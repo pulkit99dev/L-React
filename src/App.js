424be28aee3e50781b0f1e94ad9215244653503f
pulkit99dev <pulkitnagpal987654321@gmail.com>
2021-01-01 23:20:34 +0530

    Learning React-2

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Pulkit'
+  let isLoggedin = false
+
   return (
     <div className="App">
+      
+      
+        {/* function Person(props) {
+        <div className='person'>
+        <h1>{props.name}</h1>
+        <h4>My Age: {props.age}</h4>
+        </div>
+  }
+
+  let app = (
+  <div>
+  <Person name='Pulkit' age='21' />
+  <Person name='Pulkit-2' age='21' />
+  </div>
+  ); */}
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          Hello World - trial app
+          Hello{ isLoggedin ? name :'world' }
         </p>
         <a
           className="App-link"
